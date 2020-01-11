760e13a46c10ac1902dfb77aaea867d545fba41c
Cherrry9 <wisniewskijas@outlook.com>
2020-01-11 10:48:21 +0100

    changed launch script

--- config/.config/polybar/launch.sh
+++ config/.config/polybar/launch.sh
@@ -4,13 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
-  done
-else
-  polybar --reload top &
-fi
+# Launch polybar
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') WIRED=$(ls /sys/class/net/ | grep ^en | awk 'NR==1{print $1}') MONITOR=$m polybar --reload top &
+done
